7c8eff5ba2a5d2874ef303979e51c8ffcce00987
Aditya Rameshwar Bahe <adityabahe@DESKTOP-U6R63L5.localdomain>
2026-03-23 00:50:13 +0000

    added description panel

--- ui.go
+++ ui.go
@@ -3,17 +3,39 @@ package main
 
 import (
 	"fmt"
+	"image/color"
 
+	"github.com/AllenDang/giu"
 	g "github.com/AllenDang/giu"
 )
+
+// giu State variables
+var sashPos float32 = 500
+var selectedFunction int = -1
+var details string = "Hello World!!"
 
 func buildRows(messages []message) []*g.TableRowWidget {
 	rows := make([]*g.TableRowWidget, len(messages))
 	for i := range rows {
 		rows[i] = g.TableRow(
-			g.Label(fmt.Sprintf("%s", messages[i].Id)),
-			g.Label(messages[i].Name),
+			g.Selectable(messages[i].Id).
+				Selected(selectedFunction == i).
+				OnClick(func() {
+					selectedFunction = i
+					details = "Selected:" + messages[i].Id
+					fmt.Println("Selected:", messages[i].Id)
+				}),
+			g.Selectable(messages[i].Name).
+				Selected(selectedFunction == i).
+				OnClick(func() {
+					selectedFunction = i
+					details = "Selected:" + messages[i].Id
+					fmt.Println("Selected:", messages[i].Id)
+				}),
 		)
+		if selectedFunction == i {
+			rows[i].BgColor(&color.RGBA{80, 120, 200, 100})
+		}
 	}
 
 	return rows
@@ -30,8 +52,32 @@ func loop(msgchan chan message, messages []message) []message {
 		}
 	}
 render:
+
 	g.SingleWindow().Layout(
-		g.Table().Freeze(0, 1).FastMode(true).Columns(g.TableColumn("Id"), g.TableColumn("Function Name")).Rows(buildRows(messages)...),
+		g.SplitLayout(
+			g.DirectionVertical,
+			&sashPos,
+			g.Layout{
+				g.Style().
+					SetFontSize(15).
+					To(
+						g.Label("Function List"),
+					),
+				g.Separator(),
+				g.Table().Freeze(0, 1).FastMode(true).Columns(g.TableColumn("Id"), g.TableColumn("Function Name")).Rows(buildRows(messages)...),
+			},
+			g.Layout{
+				g.Style().
+					SetFontSize(15).
+					To(
+						g.Label("Description"),
+					),
+				g.Separator(),
+				g.InputTextMultiline(&details).
+					Size(-1, 200).
+					Flags(giu.InputTextFlagsReadOnly).ID("Desc"),
+			},
+		),
 	)
 
 	return messages
